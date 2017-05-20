516f55e9e3a4d404b39b4c00161d468ae0a6c4af
Erica <eberghma@ulb.ac.be>
2017-05-20 13:02:48 +0200

    processing data

---
+++ featureSelection.R
@@ -0,0 +1,17 @@
+filter <- function(X,Y){
+	N<-nrow(X)
+  	n<-ncol(X)
+
+}
+
+wrapper <- function(X,Y){
+	N<-nrow(X)
+  	n<-ncol(X)
+
+}
+
+embedded <- function(X,Y){
+	N<-nrow(X)
+  	n<-ncol(X)
+
+}
