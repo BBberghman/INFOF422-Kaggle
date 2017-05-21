457f5adf09048e8fe776f64b1fdc7e2bef6b276d
Erica <eberghma@ulb.ac.be>
2017-05-21 16:16:15 +0200

    feature selection debugging

--- functions/mRMR.R
+++ functions/mRMR.R
@@ -6,16 +6,32 @@ mrmr <- function(X,Y){
     CV.err<-matrix(0,nrow=n,ncol=10)
 
     for (i in 1:10) {
+        # testing set
         i.ts<-(((i-1)*size.CV+1):(i*size.CV))  
         X.ts<-X[i.ts,]  
         Y.ts<-Y[i.ts]  
-
+        
+        # training set
         i.tr<-setdiff(1:N,i.ts)
         X.tr<-X[i.tr,]
         Y.tr<-Y[i.tr]
-
-
-        correlation<-abs(cor(X.tr,Y.tr))
+        
+        # scaling factors
+        X.center <- colMeans(X.tr)
+        X.scale <- apply(X.tr,2,sd)
+        Y.center <- mean(Y.tr)
+        Y.scale <- sd(Y.tr)
+        
+        #scaling training set 
+        X.tr <-data.frame(scale(X.tr))
+        Y.tr <- scale(Y.tr) 
+            
+        #scaling the testing test by the same scaling as of the training set
+        X.ts <- t(apply(sweep(X.ts,2,X.mean,"-"), 1, function(x) x/X.scale))
+        X.ts <-data.frame(X.ts)
+        
+        #correlation on training set
+        correlation <- abs(cor(X.tr,Y.tr))
         correlation[is.na(correlation)] <- 0
 
         selected<-c()
@@ -40,16 +56,23 @@ mrmr <- function(X,Y){
         ranking<-selected
 
         for (nb_features in 1:n) {
-            DS<-cbind(X.tr[,ranking[1:nb_features],drop=F],imdb_score=Y.tr)
-            model <- lm(imdb_score~.,DS)
-
+            #create model
+            DS<-cbind(X.tr[,ranking[1:nb_features],drop=F],SalePrice=Y.tr)
+            model <- lm(SalePrice~.,DS)
+            
+            # infere value of Y.ts
             Y.hat.ts <- predict(model,X.ts[,ranking[1:nb_features],drop=F])
 
-            CV.err[nb_features,i] <- mean((Y.hat.ts-Y.ts)^2)
+            # unscale Y.hat.ts
+            Y.hat.ts <- cbind((Y.hat.ts*Y.scale) + Y.center)
+            Y.hat.ts[Y.hat.ts <= 0] <- 1
+
+            # compute log error
+            CV.err[nb_features,i] <- sqrt( mean( (log(Y.hat.ts)-log(Y.ts))^2 ))    
         }
     }  
 
-    #print(paste("#Features: ",c(1:n)," ; CV error=",round(apply(CV.err,1,mean),digits=4), " ; std dev=",round(apply(CV.err,1,sd),digits=4)))
+    print(paste("#Features: ",c(1:n)," CV error=",round(apply(CV.err,1,mean),digits=4), " std dev=",round(apply(CV.err,1,sd),digits=4)))
     
     nb_features.best <- which.min(round(apply(CV.err,1,mean),digits=4))
     
--- functions/wrapper.R
+++ functions/wrapper.R
@@ -5,29 +5,72 @@ wrapper <- function(X,Y){
 
     selected<-NULL
 
-    for (round in 1:n) { 
+    for (round in 1:n) 
+    { 
         candidates<-setdiff(1:n,selected)
 
         CV.err<-matrix(0,nrow=length(candidates),ncol=10)
-
-        for (j in 1:length(candidates)) {
+        
+        for (j in 1:length(candidates)) 
+        {
             features_to_include<-c(selected,candidates[j])
+            print(colnames(X[features_to_include]))
+            
+            for (i in 1:10) 
+            {
+                # testing set
+                i.ts<-(((i-1)*size.CV+1):(i*size.CV))
+                X.ts<-X[i.ts,features_to_include,drop=F] 
+                Y.ts<-Y[i.ts] 
 
-            for (i in 1:10) {
-                i.ts<-(((i-1)*size.CV+1):(i*size.CV))  
-                X.ts<-X[i.ts,features_to_include,drop=F]  
-                Y.ts<-Y[i.ts]  
+                # training set
+                i.tr<-setdiff(1:N,i.ts)
+                X.tr<-X[i.tr,features_to_include,drop=F]
+                Y.tr<-Y[i.tr]
 
-                i.tr <- setdiff(1:N,i.ts)
-                X.tr <- X[i.tr,features_to_include,drop=F]
-                Y.tr <- Y[i.tr]
+                # scaling factors
+                if (is.null(dim(X.tr))){ # if X.tr is a vector
+                    X.center <- mean(X.tr)
+                    X.scale <- sd(X.tr)
+                }    
+                else{  # if X.tr is a matrix
+                    X.center <- colMeans(X.tr)
+                    X.scale <- apply(X.tr,2,sd)                
+                }
+                Y.center <- mean(Y.tr)
+                Y.scale <- sd(Y.tr)  
 
-                DS <- cbind(X.tr,imdb_score=Y.tr)
-                model <- lm(imdb_score~.,DS)
+                #scaling training set 
+                X.tr <-data.frame(scale(X.tr))
+                Y.tr <- scale(Y.tr) 
 
-                Y.hat.ts <- predict(model,X.ts)
+                #scaling the testing test by the same scaling as of the training set
+                if (is.null(dim(X.ts))){  # if X.ts is a vector
+                    X.ts <- cbind(X.ts - X.mean)/X.scale
+                }
+                else {  # X.ts is a matrix
+                    X.ts <- t(apply(sweep(X.ts,2,X.mean,"-"), 1, function(x) x/X.scale))
+                    X.ts <-data.frame(X.ts)
+                }
+                colnames(X.ts) <- colnames(X[features_to_include])
+                colnames(X.tr) <- colnames(X[features_to_include])
 
-                CV.err[j,i] <- mean((Y.hat.ts-Y.ts)^2)
+                #print(X.ts[1:2,])
+                                
+                # create model
+                DS <- cbind(X.tr, SalePrice=Y.tr)
+                model <- lm(SalePrice~.,DS)
+                
+                #infere value of Y.ts
+                Y.hat.ts <- predict(model, X.ts)
+
+                # unscale Y.hat.ts
+                Y.hat.ts <- cbind((Y.hat.ts*Y.scale) + Y.center)
+                Y.hat.ts[Y.hat.ts <= 0] <- 1
+
+                # compute log error
+                CV.err[j,i] <- sqrt( mean( (log(Y.hat.ts)-log(Y.ts))^2 ))     
+                    
             }
         }
         
@@ -37,11 +80,11 @@ wrapper <- function(X,Y){
         selected_current <- which.min(CV.err.mean)              
         selected <- c(selected,candidates[selected_current])
         
-        print(paste("Round ",round," ; Selected feature: ",candidates[selected_current]," ; CV error = ",round(CV.err.mean[selected_current],digits=4), " ; std dev = ",round(CV.err.sd[selected_current],digits=4)))
+        print(paste("Round ",round," ; Selected feature: ",candidates[selected_current]," CV error = ",round(CV.err.mean[selected_current],digits=4), "  std dev = ",round(CV.err.sd[selected_current],digits=4)))
     
     }
     
-    nb_features.best <- which.min(round(CV.err.mean[selected_current],digits=4))
-    print(nb_features.best)
- return(selected[1:nb_features.best)                 
+    #nb_features.best <- which.min(round(CV.err.mean,digits=4))
+    #print(nb_features.best)
+ return(selected)                 
 }
